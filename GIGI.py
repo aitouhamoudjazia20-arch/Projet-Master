2e0f88a09498d60b4ae9d4402fc1bafed8f69dd6
aitouhamoudjazia20-arch <aitouhamoudjazia20@gmail.com>
2026-04-09 10:40:58 +0100

    Add files via upload

---
+++ GIGI.py
@@ -0,0 +1,35 @@
+# To read the txt file 
+import pandas as pd
+
+GAIN_path = "GAIN.txt"
+GAIN_df = pd.read_csv(GAIN_path, delim_whitespace=True, comment='E', header=None, skiprows=1)
+GAIN_df.columns = ['H(cm)', 'B(cm^-2)', 'K_eff', 'Taux', 'Taux(coup/s)', 'Gain']
+
+# Plot (K)
+import matplotlib.pyplot as plt
+plt.figure(figsize=(8, 6))
+plt.plot(GAIN_df['K_eff'], GAIN_df['Gain'], 'o-', color='purple', label='Le Gain')
+
+plt.xlabel("K_eff")
+plt.ylabel("Gain")
+plt.title("Variation du Gain en fonction du facteur de multiplication effectif ")
+plt.legend()
+plt.grid(False)
+
+outpath = "GAIN1.png"
+plt.savefig(outpath, dpi=200, bbox_inches='tight')
+plt.show()
+
+# Plot (H)
+plt.figure(figsize=(8, 6))
+plt.plot(GAIN_df['H(cm)'], GAIN_df['Gain'], 'o-', color='red', label='Le Gain')
+
+plt.xlabel("H (cm)")
+plt.ylabel("Gain")
+plt.title("Variation du Gain en fonction de la hauteur ")
+plt.legend()
+plt.grid(False)
+
+outpath = "GAIN2.png"
+plt.savefig(outpath, dpi=200, bbox_inches='tight')
+plt.show()
